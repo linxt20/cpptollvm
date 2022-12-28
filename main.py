0e823d30133013f4751d2681cec4d019e045df52
linxt20 <lxt135468@163.com>
2022-12-28 11:14:53 +0800

    补充个参数

--- main.py
+++ main.py
@@ -11,6 +11,19 @@ from llvmlite.ir.values import GlobalVariable, ReturnValue
 from table import *
 import llvmlite.ir as ir
 
+double = ir.DoubleType()
+int1 = ir.IntType(1)
+int8 = ir.IntType(8)
+int16 = ir.IntType(16)
+int32 = ir.IntType(32)
+int64 = ir.IntType(64)
+void = ir.VoidType()
+
+int8p = ir.PointerType(int8)
+int16p = ir.PointerType(int16)
+int32p = ir.PointerType(int32)
+int64p = ir.PointerType(int64)
+doublep = ir.PointerType(double)
 
 class NewCpp14Visitor(cpp14Visitor):
     def __init__(self):
